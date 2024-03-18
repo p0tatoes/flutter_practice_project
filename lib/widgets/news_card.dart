7d299e2790eb31d7f905c55be3ac7aa6a30712a3
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2024-03-18 12:10:26 +0800

    feat: added url links to original articles on news card
    - i forgor to add them ncrseitnoypwul

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -1,11 +1,21 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Container newsCard(
     {required String source,
     required String link,
     required String og,
     required String title}) {
+  Future<void> _redirectToBrowser(String url) async {
+    Uri uri = Uri.parse(url);
+    try {
+      launchUrl(uri, mode: LaunchMode.externalApplication);
+    } catch (e) {
+      throw Exception("Failed to redirect");
+    }
+  }
+
   return Container(
     width: 500.0,
     height: 300.0,
@@ -37,7 +47,17 @@ Container newsCard(
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
                   ),
-                  Text(source)
+                  Text(source),
+                  InkWell(
+                    child: Text(
+                      link,
+                      style: TextStyle(
+                          color: Colors.grey[300], fontStyle: FontStyle.italic),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () => _redirectToBrowser(link),
+                  )
                 ],
               ),
             ),
